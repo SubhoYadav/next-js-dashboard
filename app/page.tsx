bc59fded3b68f44a1757d660fb01b88cf6affcd9
Subho Yadav <subho@gide.ai>
2023-12-16 20:18:28 +0530

    Final Working copy

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import styles from '@/app/ui/home.module.css';
+import Image from "next/image"
 
 export default function Page() {
   return (
@@ -28,9 +29,26 @@ export default function Page() {
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          {/* Add Hero Images Here */}
+          <Image
+            src="/hero-desktop.png"
+            width={1000}
+            height={700}
+            className="hidden md:block"
+          />
+          <Image
+            src="/hero-mobile.png"
+            width={500}
+            height={620}
+            className="block md:hidden"
+          />
         </div>
       </div>
     </main>
   );
 }
+
+/**
+  Image optimization is itself a topic for specialisation 
+  => Different images for different devices
+  => Preventing layout shifts
+ */
